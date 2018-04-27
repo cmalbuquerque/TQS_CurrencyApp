9aaa636e118d978e307f67661cfab2fb403b13e0
Carolina Albuquerque <carolinaalbuquerque@ua.pt>
2018-04-27 23:45:30 +0100

    Selenium Test Success

---
+++ CurrencyAPI/src/test/java/homework/tests/SeleniumTest.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework.tests;
+
+/**
+ *
+ * @author carolina albuquerque
+ */
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testCaroina() throws Exception {
+    driver.get("http://localhost:8080/CurrencyAPI/");
+    driver.findElement(By.name("j_idt5:j_idt8")).clear();
+    driver.findElement(By.name("j_idt5:j_idt8")).sendKeys("1");
+    driver.findElement(By.id("j_idt5")).submit();
+    new Select(driver.findElement(By.id("j_idt5:curreciesFrom"))).selectByVisibleText("EUR");
+    driver.findElement(By.id("j_idt5:curreciesFrom")).click();
+    driver.findElement(By.id("j_idt5:j_idt6_content")).click();
+    new Select(driver.findElement(By.id("j_idt5:curreciesTo"))).selectByVisibleText("USD");
+    driver.findElement(By.id("j_idt5:curreciesTo")).click();
+    driver.findElement(By.name("j_idt5:j_idt14")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
